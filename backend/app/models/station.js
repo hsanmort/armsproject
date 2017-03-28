cfaadf6107bd8d2c63332d0568ee1130d08b7ac5
hsanmort <hassan.akrout@gmail.com>
2017-03-28 12:35:40 +0200

    model station

---
+++ backend/app/models/station.js
@@ -0,0 +1,25 @@
+/**
+ * Created by GMI-PC 
+ */
+// Station, Model
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var StationSchema =new Schema({
+    name: {type: String, index:true, required: true},
+    image: {type: String},
+    description:{type: String},
+
+    Pos:{
+
+        lat: { type: Number, required: true },
+        lng: { type: Number, required: true}
+
+    }
+});
+
+
+
+
+module.exports = mongoose.model('Station', AdminSchema);
