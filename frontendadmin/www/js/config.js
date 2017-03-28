da7cf3c9ad1a84b2b6d9ae66d759000dc02d6d22
mohamedchaar <mohamed.chaar@sesame.com.tn>
2017-03-28 11:20:13 +0100

    config.js
    add roles to config

--- frontendadmin/www/js/config.js
+++ frontendadmin/www/js/config.js
@@ -1,8 +1,19 @@
 angular.module('starter.config', [])
 .constant('WORDPRESS_API_URL', 'https://wordpress.startapplabs.com/blog/api/')
+
 .constant('GCM_SENDER_ID', '574597432927')
+
 .constant('AUTH_EVENTS', {
-  notAuthenticated: 'auth-not-authenticated'
+  notAuthenticated: 'auth-not-authenticated',
+  notAuthorized: 'auth-not-authorized'
+})
+
+.constant('USER_ROLES', {
+  admin: 'admin_role',
+  adminvoyage: 'admin_role',
+  adminpaiement: 'adminpayrole_role',
+  guichetier: 'guichetier_role',
+  chefsecteur: 'chefsecteur_role'
 })
 
 .constant('API_ENDPOINT', {
