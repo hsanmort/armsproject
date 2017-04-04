2bf4e30255266763662d63364a9f5240e421a52c
mohamedchaar <mohamed.chaar@sesame.com.tn>
2017-04-04 10:17:50 +0100

    fusion du compte paiement avec voyageur
    frontend admin guichetier modifié et optimisé ( modification scss faite plus l'extraction des données)

--- backend/app/models/voyageur.js
+++ backend/app/models/voyageur.js
@@ -22,8 +22,13 @@ var VoyageurSchema =new Schema({
         , password: { type: String, required: true}
         
         , role : { type: String, required: true}
+    },
+    ComptePaiement:{
+        exist: {type: Boolean, required: true}
 
-
+        , NumberCard: {type: Number, unique: true }
+        
+        , solde: {type: Number, required: true}
     }
 });
 VoyageurSchema.pre('save', function (next) {
